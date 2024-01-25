e2a51cb4fd99b2684d332a3cd1c109ecd4a5135a
zakariaAmeen2020 <136131826+zakariaAmeen2020@users.noreply.github.com>
2024-01-25 03:14:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './redux/store'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
 //ReactDOM.render(
 //ReactDOM.render(
     <Provider store={store}>
